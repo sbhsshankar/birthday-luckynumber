36128eb38e22202d6a3bc7738617575e4e02b854
Sbhss <shankarpulaparthi1216@gmail.com>
2023-06-04 16:06:22 +0530

    Calculate sum

--- app.js
+++ app.js
@@ -2,8 +2,24 @@ const dateOfBirth = document.querySelector("#birth-day");
 const luckyNumber = document.querySelector("lucky-number");
 const buttonCheck = document.querySelector("#button-check");
 
-function luckyOrNot(){
-
-};
 
 buttonCheck.addEventListener("click", luckyOrNot);
+
+
+function luckyOrNot(){
+const dob = dateOfBirth.value;
+const sum = calculateSum(dob);
+console.log(sum);
+};
+
+function calculateSum(dob){
+    dob = dob.replaceAll("-","");
+    let sum = 0;
+    for(let i=0; i<dob.length; i++){
+        sum = sum + Number(dob.charAt(i));
+    }
+return sum;
+}
+
+
+
