4a84d15c025fc4059e4dfc4a1e47c4d0ba08c1de
Sbhss <shankarpulaparthi1216@gmail.com>
2023-06-04 15:36:25 +0530

    Input done

---
+++ app.js
@@ -0,0 +1,9 @@
+const dateOfBirth = document.querySelector("#birth-day");
+const luckyNumber = document.querySelector("lucky-number");
+const buttonCheck = document.querySelector("#button-check");
+
+function luckyOrNot(){
+
+};
+
+buttonCheck.addEventListener("click", luckyOrNot);
